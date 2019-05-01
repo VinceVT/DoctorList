bb301956461730f2fdfff95de4f3ec30641f3da0
VinceVT <vincevutruong@gmail.com>
2019-05-01 16:37:10 -0700

    filtered everything

--- src/DoctorComponent.js
+++ src/DoctorComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import fetch from 'isomorphic-fetch';
 
 export default class Doctor extends React.Component {
@@ -7,45 +7,74 @@ export default class Doctor extends React.Component {
         super(props);
         this.state = {
             data: [],
-            selected: 1
+            selected: 0
         }
     }
     
     componentDidMount() {
-        this.fetchDate()
+        this.fetchDoctor()
     }
 
-    fetchDate = async () => {
+    fetchDoctor = async () => {
         const data = await fetch('/doctor.json')
             .then(response => response.json())
             .catch(error => console.log(error));
 
         this.setState({
-            data: data
+            data: data,
         })
     }
 
     handleChange = (event) => {
-        this.setState({ selected: event.target.value });
-        console.log(this.state.selected)
-        console.log(this.state.data.firstName)
+        console.log(event.target.value)
+        this.setState({ 
+            selected: event.target.value,
+         });
+        
     }
 
     render() {
+        //Specialty is based of Expertise displays all doctors that have same expertise as a suggestion
+        const { data, selected } = this.state;
+        const styles = {
+            display: "flex",
+            flexFlow: "column wrap",
+            justifyContent: "center",
+            alignContent: "center",
+            marginLeft: "5px"
+        };
+
+        let docArray = data.filter(sel => {
+            return sel.specialty === data[selected].specialty;
+        })
+
+        console.log(docArray);
         return (
-            <div>
+            <div style={ styles }>
                 <select value={this.state.selected} onChange={this.handleChange}>{ 
-                        this.state.data.map((doctor) => { 
-                            return <option value={doctor.id} key={doctor.id}>{doctor.firstName + " " + doctor.lastName}</option>
+                        this.state.data.map((doctor, index) => { 
+                            return <option key={doctor.id} value={index} >{doctor.firstName + " " + doctor.lastName}</option>
                         })
                     }</select>
-                <div>
-                    <ul>First Name: {this.state.data.firstName}</ul>
-                    <ul>Last Name: {this.state.data.lastName}</ul>
-                    <ul>Area Code: {this.state.data.area}</ul>
-                    <ul>Phone Number: {this.state.data.number}</ul>
-                    <ul>Expertise: {this.state.data.specialty}</ul>
-                </div>
+                <Fragment>
+                    {
+                        data[selected] && (
+                            <Fragment>
+                                <ul>First Name: {data[selected].firstName}</ul>
+                                <ul>Last Name: {data[selected].lastName}</ul>
+                                <ul>Area Code: {data[selected].area}</ul>
+                                <ul>Phone Number: {data[selected].number}</ul>
+                                <ul>Rating: {data[selected].rating}</ul>
+                                <ul>Expertise: {data[selected].specialty}</ul>
+                                <ul>Similiar Doctors: {
+                                    docArray.filter(sel => {
+                                        return sel.lastName !== data[selected].lastName
+                                    }).map(d => <div>{d.firstName + " " + d.lastName}</div>)
+                                }</ul>
+                            </Fragment>
+                        )
+                    }
+                </Fragment>
             </div>
         )
     }
